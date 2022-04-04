9232d3bad11e52946d506ecfa5a57ad506b8dd23
gabriel090 <gabriel.mbatha@gmail.com>
2022-04-04 22:05:06 +0300

    Variables

--- my_app.py
+++ my_app.py
@@ -1,2 +1,11 @@
 name = input('What is your name? ')
 print('Hello ' + name)
+# Create a variable
+name1 = 'Jamila'
+age = 18
+pi = 3.14
+cars = ['bmw', 'mercedes','range rover']
+
+print(name1)
+print(pi)
+print(cars)
